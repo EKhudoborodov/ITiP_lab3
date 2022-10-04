84a81ed5b85ed96c3170ca990fd0670f5f4a1520
EKhudoborodov <vakros.vakros@mail.ru>
2022-10-04 22:09:33 +0300

    result

--- Primes.java
+++ Primes.java
@@ -9,18 +9,18 @@ Write your code in this editor and press "Run" button to execute it.
 public class Primes
 {
 	public static void main(String[] args) {
-		for(int i=2; i<100; i++){
-		    if (isPrime(i)){
-		        System.out.println(i);
+		for(int i=2; i<100; i++){ // Перебор чисел от 2 до 100
+		    if (isPrime(i)){ // Проверка чисел на простоту
+		        System.out.println(i); // Вывод простых чисел
 		    }
 		}
 	}
 	public static boolean isPrime(int n){
-	    for(int i=2; i<n/2; i++){
-	        if (n%i==0){
-	            return false;
+	    for(int i=2; i<n/2; i++){ // Перебор чисел от 2 до n/2
+	        if (n%i==0){ // Проверка делимости n на i
+	            return false; // Число не простое
 	        }
 	    }
-	    return true;
+	    return true; // Число простое
 	}
 }
